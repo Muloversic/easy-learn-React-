5ccb140ccd5b2f08b2672f2664ac00a5e3d32436
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-03-25 10:11:54 +0200

    created markup for newSet component

--- src/components/NewSet.js
+++ src/components/NewSet.js
@@ -1,4 +1,17 @@
+import { React, useState, useEffect } from "react";
 export default function NewSet(props) {
+  useEffect(() => {
+    const wordElemArr = document.querySelectorAll(".form__word");
+    console.log(wordElemArr);
+    [...wordElemArr].forEach((wordElement) => {
+      const buttonAdd =
+        '<button class="material-icons">add_circle_outline</button>';
+      if (wordElement.nextElementSibling === null) {
+        wordElement.insertAdjacentHTML("afterend", buttonAdd);
+      }
+    });
+  });
+
   return (
     <main className="main main-new_set new-set">
       <form className="new-set__form form">
@@ -24,23 +37,34 @@ export default function NewSet(props) {
         />
         <div className="form__words">
           <div className="form__word">
-            <div className="from__word-input">
-              <label className="form__label">Term</label>
-              <input
-                type="text"
-                placeholder="Term"
-                name="Term"
-                className="form__input"
-              />
-            </div>
-            <div className="from__word-input">
-              <label className="form__label">Determination</label>
-              <input
-                type="text"
-                placeholder="Determination"
-                name="Determination"
-                className="form__input"
-              />
+            <div className="form__word-data">
+              <div className="form__word-input">
+                <label className="form__label form__label--new-word">
+                  Term
+                </label>
+                <input
+                  type="text"
+                  name="Term"
+                  className="form__input form__input--new-word"
+                />
+                <button className="form__btn-lang">Choose language</button>
+              </div>
+              <div className="form__word-input">
+                <label className="form__label form__label--new-word">
+                  Determination
+                </label>
+                <input
+                  type="text"
+                  name="Determination"
+                  className="form__input form__input--new-word"
+                />
+                <div className="form__buttons-lang">
+                  <button className="form__btn-lang">
+                    Add one more language
+                  </button>
+                  <button className="form__btn-lang">Choose language</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
