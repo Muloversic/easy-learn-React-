056af610cb28d47e8b0c0d559ae17cda49a47ed4
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-03-31 10:35:20 +0300

    data from db is saved to state

--- src/components/Sets.js
+++ src/components/Sets.js
@@ -1,8 +1,21 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
 export default function Sets() {
-    
-  return (
-    <div className="sets">
-     
-    </div>
-  );
+  const db = getDatabase();
+  const sets = ref(db, "sets/");
+  const [data, setData] = useState();
+  useEffect(() => {
+    onValue(sets, (snapshot) => {
+      const data = snapshot.val();
+      setData((prevData) => {
+        return {
+          ...prevData,
+          data,
+        };
+      });
+    });
+  }, []);
+
+  console.log(data);
+  return <div className="sets"></div>;
 }
