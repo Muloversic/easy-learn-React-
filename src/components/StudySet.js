065307803a17cbbfcd98274e84a02c79cceccd18
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-04-05 14:08:13 +0300

    data from inputs at learning page can be stored

--- src/components/StudySet.js
+++ src/components/StudySet.js
@@ -1,31 +1,76 @@
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 export default function StudySet({ wordsToLearn }) {
-  const wordElement = wordsToLearn.data.map((word) => {
+  const [inputData, setInputData] = useState([]);
+  const [wordsData, setWordsData] = useState([]);
+  function getData(target, id) {
+    if (target.name === "Determination") {
+      setInputData((prevData) => {
+        return {
+          ...prevData,
+          translation: target.value,
+          id: id,
+        };
+      });
+    }
+  }
+
+  useEffect(() => {
+    setWordsData((prevData) => {
+      return {
+        ...prevData,
+        [inputData.id]: inputData,
+      };
+    });
+  }, [inputData]);
+
+  function filterWordsData() {
+    const allData = [];
+    for (let data in wordsData) {
+      allData.push(wordsData[data]);
+    }
+
+    const tmpArray = [];
+    function itemCheck(item) {
+      if (tmpArray.indexOf(item.id) === -1) {
+        tmpArray.push(item.id);
+        return true;
+      }
+      return false;
+    }
+
+    const data = allData.reverse().filter((item) => item.id !== undefined);
+    const dataReady = data.filter((item) => itemCheck(item));
+    return dataReady.reverse();
+  }
+
+  const wordsElements = wordsToLearn.data.map((word) => {
     return (
-      <div className="study__word" key={nanoid()}> 
-        <label className="study__label">Term: {word.word}</label>
+      <div className="study__word">
+        <label className="study__word-label">{word.word}</label>
         <input
           type="text"
-          name="translation"
-          className="study__input"
-          placeholder="Translation"
-          //   onChange={(event) => getData(event.target, props.id)}
+          name="Determination"
+          className="study__word-input"
+          onChange={(event) => getData(event.target, word.id)}
         />
-        {word.extraTranslation && <input
-          type="text"
-          name="extraTranslation"
-          className="study__input"
-          placeholder="extraTranslation"
-          //   onChange={(event) => getData(event.target, props.id)}
-        />}
       </div>
     );
   });
 
+
+
+  function checkAnswers(event) {
+    event.preventDefault();
+    console.log(filterWordsData());
+  }
+
   return (
-    <main className="study">
-      <div className="study__words">{wordElement}</div>
-      <button className="study__button">Confirm</button>
+    <main>
+      <form className="study" onSubmit={checkAnswers}>
+        <div className="study__elements">{wordsElements}</div>
+        <button className="study__button">Submit answers</button>
+      </form>
     </main>
   );
 }
