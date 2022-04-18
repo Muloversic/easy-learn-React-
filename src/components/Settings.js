99b983329dd3d8b455c7aa9e5eb8afa1650bbad7
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-04-18 14:08:29 +0300

    created custom style for input=file

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,5 +1,28 @@
-export default function Settings(){
-    return(
-        <h1>Settgins</h1>
-    )
+export default function Settings() {
+  return (
+    <main className="main">
+      <div className="main__settings settings">
+        <div className="settings__user">
+          <h2 className="settings__user-title">
+          Choose profile photo and wirte your name
+          </h2>
+          <div className="settings__input-wrapper">
+            <input
+              name="file"
+              type="file"
+              id="input__file"
+              className="settings__input-file"
+              accept="image/*"
+            />
+            <label htmlFor="input__file" className="settings__input-button">
+              <span className="settings__icon-wrapper">
+              <i className="settings__icon-file Small material-icons">arrow_downward</i>
+              </span>
+              <span className="settings__input-text">Choose file</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
