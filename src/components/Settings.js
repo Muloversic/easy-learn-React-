b96c9225241c09214dff0198e10f060876a798e2
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-04-18 14:55:47 +0300

    started to write function handle input=file

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,4 +1,11 @@
 export default function Settings() {
+  const inputFile = document.querySelector(".settings__input-file");
+  const preview = document.querySelector(".settings__img-preview");
+
+  function handleFiles(files) {
+    console.log(files);
+    
+  }
   return (
     <main className="main">
       <div className="main__settings settings">
@@ -7,7 +14,9 @@ export default function Settings() {
             Choose profile photo and wirte your name
           </h2>
           <div className="settings__user-data">
-            <label htmlFor="userName" className="settings__user-name">Write your name here</label>
+            <label htmlFor="userName" className="settings__user-name">
+              Write your name here
+            </label>
             <input type="text" className="settings__input" id="userName" />
           </div>
           <div className="settings__input-wrapper">
@@ -17,6 +26,7 @@ export default function Settings() {
               id="input__file"
               className="settings__input-file"
               accept="image/*"
+              onChange={handleFiles}
             />
             <label htmlFor="input__file" className="settings__input-button">
               <span className="settings__icon-wrapper">
@@ -26,6 +36,7 @@ export default function Settings() {
               </span>
               <span className="settings__input-text">Choose file</span>
             </label>
+            <div className="settings__img-preview"></div>
           </div>
         </div>
       </div>
