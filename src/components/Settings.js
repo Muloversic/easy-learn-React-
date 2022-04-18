e7d1c0d0ad42aa512a7189b6d2af0a3ce54f3a28
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-04-18 14:40:03 +0300

    wrote some styles for settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -4,8 +4,12 @@ export default function Settings() {
       <div className="main__settings settings">
         <div className="settings__user">
           <h2 className="settings__user-title">
-          Choose profile photo and wirte your name
+            Choose profile photo and wirte your name
           </h2>
+          <div className="settings__user-data">
+            <label htmlFor="userName" className="settings__user-name">Write your name here</label>
+            <input type="text" className="settings__input" id="userName" />
+          </div>
           <div className="settings__input-wrapper">
             <input
               name="file"
@@ -16,7 +20,9 @@ export default function Settings() {
             />
             <label htmlFor="input__file" className="settings__input-button">
               <span className="settings__icon-wrapper">
-              <i className="settings__icon-file Small material-icons">arrow_downward</i>
+                <i className="settings__icon-file Small material-icons">
+                  arrow_downward
+                </i>
               </span>
               <span className="settings__input-text">Choose file</span>
             </label>
