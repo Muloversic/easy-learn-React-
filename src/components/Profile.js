3a83571ef96271f6736b7ebdb04d63cf914148c9
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-04-18 12:02:03 +0300

    created main markup and styles for profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,23 @@
-export default function Profile(){
-    return(
-        <main>
-            <p>profile page</p>
-        </main>
-    )
+export default function Profile() {
+  return (
+    <main className="main">
+      <div className="main__profile profile">
+        <div className="profile__user">
+          <img className="profile__user-photo" src="../images/IMG_20220403_103746.jpg" alt="user" />
+          <p className="profile__user-name">Oleh Nikulin</p>
+        </div>
+        <div className="profile__statistic">
+          <h2 className="profile__statistic-title">Your statistics</h2>
+          <ul className="profile__list">
+            <li className="profile__list-item">Words in total:</li>
+            <li className="profile__list-item">Learned words:</li>
+            <li className="profile__list-item">Amount of sets:</li>
+            <li className="profile__list-item">
+              Average percent of sets progress:
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
 }
